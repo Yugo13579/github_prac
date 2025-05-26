3b47bf9cf88c8d38a22461ef69e4ad79a7d44eb9
Yugo13579 <yuhaseg30@icloud.com>
2025-05-26 19:19:31 +0900

    git-pracをmerge

--- script.js
+++ script.js
@@ -3,17 +3,9 @@ const decrement = document.getElementsByTagName("button")[1];
 const display = document.getElementsByTagName("h2")[0];
  
 increment.addEventListener("click", (e) => {
-<<<<<<< HEAD
-    display.textContent = Number(display.textContent) + 10;
-});
- 
-decrement.addEventListener("click", (e) => {
-    display.textContent = Number(display.textContent) - 10;
-=======
     display.textContent = Number(display.textContent) + 2;
 });
  
 decrement.addEventListener("click", (e) => {
     display.textContent = Number(display.textContent) - 2;
->>>>>>> git-prac
 });
